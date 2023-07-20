83202945761a911d2dd79633032da3ffcb4e18c7
Wesley Oliveira <wesleysistemaor@gmail.com>
2023-07-20 11:44:10 -0300

    PRIMO0.1

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainKtTest {
+
+    @Test
+    fun main() {
+        val result = main(7)
+        assertTrue(result)
+    }
+}
